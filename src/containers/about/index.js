810a4eb4d532ba5aaa9dc3e2de91aea57e1bdb7b
alvaro <alvaro@flippercommunity.com>
2018-02-11 20:08:54 +0000

    Update About page

--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import './about.css'
 
 export default () => (
-  <div>
-    <h1>About Us</h1>
-    <p>Hello Medium!</p>
+  <div className="about">
+    <h1>About Me</h1>
+    <p>Project develop by Alvaro Jimenez Martin (<a href="https://www.linkedin.com/in/alvarojimenezmartin/">linkedin</a>)</p>
+    <p>Source code of this on: <a href="https://github.com/AJM88/fantasysports-test/" target="blank">https://github.com/AJM88/fantasysports-test/</a></p>
   </div>
 )
