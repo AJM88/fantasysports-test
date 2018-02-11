103543968240a4fe2d0c2ed681899eb57f2b377d
alvaro <alvaro@flippercommunity.com>
2018-02-11 20:10:26 +0000

    Update navigation bar

--- src/containers/app/app.js
+++ src/containers/app/app.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom'
 import LineupField from '../lineupField';
 import About from '../about'
+import './app.css'
 
 const App = () => (
   <div>
-    <header>
-      <Link to="/">Lineup</Link>
-      <Link to="/about">About</Link>
-    </header>
+    <nav className="nav">
+      <Link className="nav-item" to="/">Lineup</Link>
+      <Link className="nav-item" to="/about">About</Link>
+    </nav>
 
     <main>
       <Route exact path="/" component={LineupField} />
